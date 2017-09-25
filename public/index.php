47d4e71cc30d72addbf35d6a949520efb527c4f4
micos7 <micos7@gmail.com>
2017-09-25 20:56:22 +0300

    Run

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php
+require '../vendor/autoload.php';
 
-$app = new App([
-
-]);
+$app = new \Framework\App();
 
 $app->run();
 
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -2,6 +2,16 @@
 namespace Framework;
 
 Class App{
+    public function run(){
+        $uri = $_SERVER['REQUEST_URI'];
+        if(!empty($uri) && $uri[-1] === "/"){
+            header('Location: '.substr($uri, 0, -1));
+            header('HTTP/1.1 301 Moved Permanently');
+            exit();
+        }
+
+        echo "Hello";
+    }
     
 }
 
